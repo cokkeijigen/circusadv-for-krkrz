e8c4340de1d7577756849c83336f555b776e2103
cokkeijigen <itsukezigen@gmail.com>
2026-06-09 07:37:48 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "ss.colytitse.kr2patch"
-        minSdk = 29
+        minSdk = 21
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
